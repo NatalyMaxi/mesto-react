e0cab251de40e6a8b5579631c4cf50dd3a462af6
Natalya Maximova <natka-31@mail.ru>
2022-07-02 11:37:56 +0500

    присвоить обработчики кнопкам

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,22 +10,20 @@ function handleEditAvatarClick() {
    document.querySelector('.popup_type_edit-avatar').classList.add('popup_is-active');
 }
 
-
-
 function Main() {
    return (
       <main className="content">
          <section className="profile">
-            <div className="profile__container-avatar">
+            <div className="profile__container-avatar" onClick={handleEditAvatarClick}>
                <button className="profile__btn-avatar"></button>
                <img className="profile__image" src="#" alt="Аватар" />
             </div>
             <div className="profile__info">
                <h1 className="profile__title">Жак-Ив Кусто</h1>
-               <button className="profile__btn" type="button" aria-label="Редактировать"></button>
+               <button className="profile__btn" type="button" aria-label="Редактировать" onClick={handleEditProfileClick}></button>
                <p className="profile__subtitle">Исследователь океана</p>
             </div>
-            <button className="profile__button" type="button" aria-label="Добавить"></button>
+            <button className="profile__button" type="button" aria-label="Добавить" onClick={handleAddPlaceClick}></button>
          </section>
          <section className="pictures">
             <ul className="list">
