3e558f7ff8ec7f441d35e4fff677d227156f1ffe
Natalya Maximova <natka-31@mail.ru>
2022-07-03 14:11:38 +0500

    исправить ошбики 1

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,7 +9,7 @@ function PopupWithForm(props) {
             <h3 className="popup__title">{props.title}</h3>
             <form
                className="form"
-               name={`${props.name}`}
+               name={props.name}
                noValidate>
                {props.children}
 
