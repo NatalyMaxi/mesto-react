64bc9231200331dab5fba8e7cee6e7ea08bb0a1f
Natalya Maximova <natka-31@mail.ru>
2022-07-13 13:01:36 +0500

    подписаться на контекст card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,39 @@
+import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
+
 function Card({ card, onCardClick }) {
+
+   const currentUserContext = React.useContext(CurrentUserContext);
 
    function handleClick() {
       onCardClick(card);
    }
-   
+
+   // Определяем, являемся ли мы владельцем текущей карточки
+   const isOwn = card.owner._id === currentUserContext._id;
+
+   // Создаём переменную, которую после зададим в `className` для кнопки удаления
+   const cardDeleteButtonClassName = (
+      `profile__delete ${isOwn ? '' : 'profile__delete_hidden'}`
+   );
+
+   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+   const isLiked = card.likes.some(i => i._id === currentUserContext._id);
+
+   // Создаём переменную, которую после зададим в `className` для кнопки лайка
+   const cardLikeButtonClassName = `element__button ${isLiked ? `element__button_active` : ``}`;
    return (
       <li className="list__items">
          <img src={card.link} alt={card.name} className="list__image" onClick={handleClick} />
          <div className="list__item">
             <h2 className="list__title">{card.name}</h2>
             <div className="list__like-container">
-               <button className="list__toggle" type="button" aria-label="Отметить"></button>
+               <button className={cardLikeButtonClassName} type="button" aria-label="Отметить"></button>
                <span className="list__like-counter">{card.likes.length}</span>
             </div>
-            <button className="list__btn" type="button" aria-label="Удалить"></button>
+            <button className={cardDeleteButtonClassName} type="button" aria-label="Удалить"></button>
          </div>
-      </li> 
+      </li>
    )
 }
 
